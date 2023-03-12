c7d95e106132453245aa6363123e009e3fc1d5ea
akira0915n3 <akira0915n3@outlook.com>
2023-03-12 09:58:56 +0900

    second commit

---
+++ sspplugin/src/main/java/sspplugin/App.java
@@ -0,0 +1,13 @@
+package sspplugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
